57b15d7560174d16f6dc48e2054408252ca12ca7
xiucai <xiucai@pahuashan.cn>
2016-11-09 18:46:24 +0800

    invoke local app, but io deadlock appears

--- core/process.py
+++ core/process.py
@@ -5,33 +5,55 @@ import subprocess
 import threading
 import Queue
 
+class CMD:
+    def __init__(self, cmd):
+        assert isinstance(cmd, basestring)
+        self.cmd = cmd
+    def __str__(self):
+        return self.cmd
+    __repr__ = __unicode__ = __str__
+
 class IMT:
-    def __init__(self, fd, pq):
-        assert hasattr(fd, 'read') or hasattr(fd, 'write')
-        assert isinstance(pq, Queue.PriorityQueue)
-        self.fd = fd
-        self.pq = pq
+    def __init__(self, p, q):
+        assert isinstance(p, subprocess.Popen)
+        assert isinstance(q, Queue.Queue)
+        self.p = p
+        self.q = q
+        self()
+    def __call__(self):
+        print 'perform %s (p=%s, q=%s)' % (self, self.p, self.q)
 
 class Manager:
-    def __init__(self, i, o, e, pq):
-        assert isinstance(i, threading.Thread)
-        assert isinstance(o, threading.Thread)
-        assert isinstance(e, threading.Thread)
-        assert isinstance(pq, Queue.PriorityQueue)
-        self.i = i
-        self.o = o
-        self.e = e
-        self.pq = pq
+    def __init__(self, ti, to, te, p, qi, qoe):
+        assert isinstance(ti, threading.Thread)
+        assert isinstance(to, threading.Thread)
+        assert isinstance(te, threading.Thread)
+        assert isinstance(p, subprocess.Popen)
+        assert isinstance(qi, Queue.Queue)
+        assert isinstance(qoe, Queue.Queue)
+        self.ti = ti
+        self.to = to
+        self.te = te
+        self.p = p
+        self.qi = qi
+        self.qoe = qoe
+        self()
+    def __call__(self):
+        print 'perform %s (ti=%s, to=%s, te=%s, p=%s, qi=%s, qoe=%s)' % (self, self.ti, self.to, self.te, self.p, self.qi, self.qoe)
 
 def invoke(cmd, indication, monitor, treatment, manager):
-    assert isinstance(indication, IMT)
-    assert isinstance(monitor, IMT)
-    assert isinstance(treatment, IMT)
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    pq = Queue.PriorityQueue()
-    stdin = threading.Thread(target=indication, p.stdin, pq)
-    stdout = threading.Thread(target=monitor, p.stdout, pq)
-    stderr = threading.Thread(target=treatment, p.stderr, pq)
-    mngr = threading.Thread(target=manager, stdin, stdout, stderr, pq)
+    assert isinstance(cmd, CMD)
+    assert issubclass(indication, IMT)
+    assert issubclass(monitor, IMT)
+    assert issubclass(treatment, IMT)
+    assert issubclass(manager, Manager)
+    p = subprocess.Popen(`cmd`, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    qi = Queue.Queue()
+    qoe = Queue.Queue()
+    ti = threading.Thread(target=indication, args=(p, qi))
+    to = threading.Thread(target=monitor, args=(p, qoe))
+    te = threading.Thread(target=treatment, args=(p, qoe))
+    mngr = threading.Thread(target=manager, args=(ti, to, te, p, qi, qoe))
     mngr.setDaemon(True)
     mngr.start()
+    mngr.join()
--- samples/download.py
+++ samples/download.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python
 #coding=utf-8
+
+from core.process import CMD, IMT, Manager, invoke
+
+class Indication(IMT):
+    def __call__(self):
+        while True:
+            data = self.q.get()
+            if data:
+                print 'indication data:', data
+                self.p.stdin.write(data)
+class Monitor(IMT):
+    def __call__(self):
+        while True:
+            data = self.p.stdout.read()
+            if data:
+                print 'monitor data:', data
+                self.q.put(data)
+class Treatment(IMT):
+    def __call__(self):
+        while True:
+            data = self.p.stderr.read()
+            if data:
+                print 'treatment data:', data
+                self.q.put(data)
+
+class Download(Manager):
+    def __call__(self):
+        self.ti.start()
+        self.to.start()
+        self.te.start()
+        self.p.wait()
+        print 'manager:', self.qoe.get()
+
+def download(cmd):
+    invoke(CMD(cmd), Indication, Monitor, Treatment, Download)
+
+if __name__ == '__main__':
+    download('scp root@192.168.10.190:/home/bob/sources .')

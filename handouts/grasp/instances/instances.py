dd39e255389485c7dd73128791b392d39b46a1a2
bob-nx <me@nagexiucai.com>
2017-11-06 16:46:53 +0800

    update instances in grasp

--- handouts/grasp/instances/instances.py
+++ handouts/grasp/instances/instances.py
@@ -21,11 +21,45 @@ class Person(object):
         self._height, self._weight = None, None # 身高体重
         self.__id = __id # 身份证号
 
+    def WhoAmI(self):
+        print self.name
+
+    def Insight(self):
+        print self._sex, self._height, self._weight
+
+    def WhoIsThat(self):
+        print self.GetId()
+
     def Walk(self): # Q：人是怎么走的呢？
         print "one two one~" # A：一二一（给出一个包袱）
 
     def Eat(self): # Q：人是怎么吃的呢？
         print "bia-ji" # A：Bia-Ji
+
+    def GetId(self):
+        # TODO: 征得本人同意或具有某种特权
+        return self.__id
+    def SetId(self, __id):
+        # TODO: 征得本人同意或具有某种特权
+        self.__id = __id
+
+    def GetSex(self):
+        # TODO: 征得本人同意或具有某种特权
+        return self._sex
+    def SetId(self, _sex):
+        # TODO: 征得本人同意或具有某种特权
+        self._sex = _sex
+
+    def GetHeight(self):
+        return self._height
+    def GetWeight(self):
+        return self._weight
+
+    def GetName(self):
+        return self.name
+    def SetName(self, name):
+        # TODO: 征得本人同意或具有某种特权
+        self.name = name
 
 class ChairMan(Person):
 
@@ -34,6 +68,10 @@ class ChairMan(Person):
 
     def Eat(self): # Q：主席怎么吃的呢？
         print "big dinner" # A：大餐
+
+    def Walk(self):
+        super(ChairMan, self).Walk() # 调用亲类方法
+        print "left right left~"
 
 class MaNong(Person):
 
@@ -47,13 +85,18 @@ class CTO(ChairMan, MaNong): # 既是领导又会写代码的就是吸啼噢
 
 person = Person("PingMin", "Any", 0)
 person.Eat()
+person.WhoAmI()
 
 chairman = ChairMan("XiDada", "Secret", 20121115)
 chairman.Eat()
+chairman.Walk()
+chairman.WhoAmI()
 
-manong = MaNong("xiucai", "PureMale", 9527)
+manong = MaNong("XiuCai", "PureMale", 9527)
 manong.Eat()
+manong.WhoAmI()
 
 cto = CTO("XiMen", "Unknown", 20160905)
 cto.Eat()
 cto.Fly()
+cto.WhoAmI()

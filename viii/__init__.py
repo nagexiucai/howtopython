db8958366dfe1d080577b2d1743fb05349c741a0
bob-nx <me@nagexiucai.com>
2018-01-04 14:15:53 +0800

    add viii

---
+++ viii/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Time    : 2018/1/3 11:59
+# @Author  : Bob
+# @Website : www.nagexiucai.com
+# @E-Mail  : me@nagexiucai.com
+# @Summary : Python V3.5.1+。
+
+import requests
+import asyncio
+
+# TODO: “async def”替代“@asyncio.coroutine”、“await”替代“yield from”
